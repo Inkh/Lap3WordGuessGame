9db4bc58c3a186ec03a4749b688057346d158bf4
inkh <jhwe92@gmail.com>
2018-10-14 15:28:29 -0700

    Init. Added file creation method and read method

---
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace WordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string wordPath = "../../../wordList.txt";
+            CreateFile(wordPath);
+            
+        }
+
+        static void CreateFile(string path)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    try
+                    {
+                        sw.Write("Your list" +
+                            "");
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                    finally
+                    {
+                        sw.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        static void DisplayCurrentWords(string path)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string word = "";
+                    while ((word = sr.ReadLine()) != null)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        static void AddMoreWords()
+        {
+
+        }
+    }
+}
